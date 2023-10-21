6579e13fbf22664836f13f49df6419ba84a9a7fc
swethapasupuleti-git <141996206+swethapasupuleti-git@users.noreply.github.com>
2023-10-21 11:09:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const statustext = document.getElementById("status");
+const enterbutton = document.getElementById("enterBtn");
+
+enterbutton.addEventListener("click", function (){
+     const h1element = document.createElement("h1");
+	 h1element.innerText = "Entered Metaverse";
+	 statustext.replaceWith(h1element);
+});
